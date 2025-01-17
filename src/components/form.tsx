0214b246d16803b7b7794c137eef9bfada971d72
= <=>
2025-01-17 18:28:16 -0300

    enviando outras informações junto com o arquivo

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useState } from "react"
 
 export const Form = ()=>{
     const [selectedFile, setSelectedFile] = useState<File>();
+    const [legend, setLegend] = useState('');
 
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>)=>{
         /* Não importa se foi escolhido só um arquivo, vai vim um array de files e dentro cada índice vem um file */
@@ -18,6 +19,7 @@ export const Form = ()=>{
         if(selectedFile){
             const formData = new FormData();
             formData.append('file', selectedFile);
+            formData.append('legend', legend)
 
             const req = await fetch('https://b7web.com.br/uploadtest/', {
                 method: 'POST',
@@ -32,6 +34,7 @@ export const Form = ()=>{
     return (
         <div>
             <input type="file" className="block my-3" onChange={handleFileChange} />
+            <input type="text" className="text-black" value={legend} onChange={e => setLegend(e.target.value)}  />
             <button className="block my-3" onClick={handleSubmit}>Enviar</button>
         </div>
     )
